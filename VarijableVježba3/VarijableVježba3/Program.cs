0ff999ed352693b1ff7211000bb707c4ee208f63
denimendica <71275614+denimendica@users.noreply.github.com>
2020-11-13 16:15:06 +0100

    Add files via upload

---
+++ VarijableVježba3/VarijableVježba3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarijableVježba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=2;
+            double b=5;
+            double c=7;
+            double d=4;
+
+            double x = (Math.Pow(a, 2) + Math.Pow(b, 2) + Math.Pow(c, 2) + Math.Pow(d, 2)) / (Math.Pow(a+b, 2) + Math.Pow(c+d, 2) );
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
